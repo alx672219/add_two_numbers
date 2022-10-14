e6cba6da2cc2f8827043bf0008d84c4c6fa912c4
Alex <113051612+alx672219@users.noreply.github.com>
2022-10-14 02:04:10 -0400

    Update add_two_numbers.py

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -6,21 +6,25 @@ from typing import ListNode
 #         self.val = val
 #         self.next = next
 class Solution:
+    
+    # ListNode, not Linked List
+    
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        res = dummy = ListNode()
+        dummy = ListNode()
+        curr = dummy
+        
         carry = 0   # 8 + 6 = 14, carry is 1
-        while l1 or l2:
-            v1, v2 = 0, 0
-            if l1: v1, l1 = l1.val, l1.next
-            if l2: v2, l2 = l2.val, l2.next
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
             
             val = carry + v1 + v2
             carry = val // 10   # 8 + 6 = 14, carry = 1
             val = val % 10      # 8 + 6 = 14, val = 4
-            res.next = ListNode(val)
-            res = res.next
+            curr.next = ListNode(val)
+            curr = curr.next
             
-        if carry:
-            res.next = ListNode(carry)
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
             
         return dummy.next
